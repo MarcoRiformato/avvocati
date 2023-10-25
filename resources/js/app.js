517064272e58b8c83d03a5e310a7e14bfa73a9b7
Marco Riformato <75418100+MarcoRiformato@users.noreply.github.com>
2023-10-25 20:06:36 +0200

    QuillEditor installed

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,17 +5,22 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { QuillEditor } from '@vueup/vue-quill';
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME;
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => appName ? `${title} - ${appName}` : title,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+        const app = createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue)
-            .mount(el);
+            .use(ZiggyVue, Ziggy);
+        app.component('QuillEditor', QuillEditor);
+        app.mount(el);
+        //themeChange(false);
+
     },
     progress: {
         color: '#4B5563',
