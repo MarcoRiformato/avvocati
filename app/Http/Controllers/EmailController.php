e467ce522312c91e2d4498d049493a34e0a6339c
Marco Riformato <75418100+MarcoRiformato@users.noreply.github.com>
2024-06-03 11:51:09 +0200

    adding email inviata

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\SendMail;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class EmailController extends Controller
 {
@@ -17,6 +18,8 @@ class EmailController extends Controller
         ];
 
         Mail::to('marco.riformato@gmail.com')->send(new SendMail($details));
-        return response()->json(['message' => 'Email successfully sent']);
+        return Inertia::render('YourComponent', [
+            'message' => 'Email inviata'
+        ]);
     }
 }
