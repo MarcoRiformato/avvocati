38fd3a242e7b80eea19243231462dc85eb880726
Marco Riformato <75418100+MarcoRiformato@users.noreply.github.com>
2024-06-04 00:41:17 +0200

    Enabling maintenance mode

--- resources/views/emails/sendEmail.blade.php
+++ resources/views/emails/sendEmail.blade.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>New Contact Request</title>
+    <title>Nuova richiesta di contatto</title>
 </head>
 <body>
-    <h1>Contact Request</h1>
-    <p>Name: {{ $details['name'] }}</p>
-    <p>Surname: {{ $details['surname'] }}</p>
+    <h1>Richiesta di contatto</h1>
+    <p>Nome: {{ $details['name'] }}</p>
+    <p>Cognome: {{ $details['surname'] }}</p>
     <p>Email: {{ $details['email'] }}</p>
-    <p>Message: {{ $details['message'] }}</p>
+    <p>Messaggio: {{ $details['message'] }}</p>
 </body>
 </html>
